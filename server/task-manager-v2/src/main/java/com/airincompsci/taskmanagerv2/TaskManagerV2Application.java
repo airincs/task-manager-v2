bfa59a8058d0f272ed0891c29b76d0ffed41fb13
AirinCompSci <airincompsci@gmail.com>
2022-06-19 12:58:47 -0400

    Initialized Spring Boot

---
+++ server/task-manager-v2/src/main/java/com/airincompsci/taskmanagerv2/TaskManagerV2Application.java
@@ -0,0 +1,13 @@
+package com.airincompsci.taskmanagerv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerV2Application.class, args);
+	}
+
+}
---
+++ server/task-manager-v2/src/test/java/com/airincompsci/taskmanagerv2/TaskManagerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airincompsci.taskmanagerv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
